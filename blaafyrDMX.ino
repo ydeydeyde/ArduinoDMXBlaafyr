88c9d78a9cbf9fd12112b11d1237b4494ff65288
ydeydeyde <35302563+ydeydeyde@users.noreply.github.com>
2019-04-07 11:14:43 +0200

    Update blaafyrDMX.ino

--- blaafyrDMX.ino
+++ blaafyrDMX.ino
@@ -1,10 +1,16 @@
+// NeoPixel library
 #include <Adafruit_NeoPixel.h>
+
+// CTC-DRA-10-R2 DMX shield library
 #include <Conceptinetics.h>
 
-#define NEOPIXEL_PIN 7
+// Digital data pin for WS2818b LED strip
+#define NEOPIXEL_PIN 6
 
 // Define number of pixels on LED strip
 #define numPixels 75
+
+// Number of DMX Channels (this be anything from 1-512)
 #define DMX_SLAVE_CHANNELS 4
 
 #define DMX_ADDRESS 1
@@ -26,17 +32,24 @@ void setup() {
   dmx_slave.setStartAddress (DMX_ADDRESS);
 
   strip.begin();
+  // Updates LED strip (now to off, since there is no value assigned yet)
   strip.show();
 }
 
 void loop() {
+  // DMX address 1
   bright = dmx_slave.getChannelValue (DMX_ADDRESS);
+  // DMX address 2
   lowerBorder = dmx_slave.getChannelValue (DMX_ADDRESS + 1);
+  // DMX adress 3
   upperBorder = dmx_slave.getChannelValue (DMX_ADDRESS + 2);;
+  
+  // Mapping incoming DMX bytes to number of pixels on strip
   lowerBorder = map(lowerBorder, 1, 255, 1, numPixels);
   upperBorder = map(upperBorder, 1, 255, 1, numPixels);
 
   int bRandom = random(255);
+  
   if (bright > 0) {
     flameSpread(strip.Color(bRandom, bRandom, bRandom));
     flameSpread(strip.Color(bright, bright, bright));
@@ -54,7 +67,7 @@ void flameSpread(uint32_t c) {
     strip.show();
   }
 }
-
+// Every 3rd iteration of the flame will be receiving a random variable, to simulate noise in the white flame
 void flameOffSet(uint32_t c) {
   for (int i = lowerBorder; i < upperBorder; i = i + 3) {
     strip.setPixelColor(i, c);
@@ -72,7 +85,9 @@ void outerBlue(uint32_t c) {
    // strip.show();
   }
   for (int l = upperBorder; l < numPixels; l++) {
+    // Trying to fade upperEnd with a sin wave (0-1)
     //float fade = sin(l / numPixels * PI);
+    // Splitting up RGB in bytes using bitshifting
     //int red = (c & 0xFF0000) >> 16;
     //int green = (c & 0xFF00) >> 8;
     //int blue = c & 0xFF;
@@ -81,7 +96,9 @@ void outerBlue(uint32_t c) {
     strip.show();
   }
   for (int m = 0; m < 15; m++) {
+    // Trying to fade lowerEnd with a sin wave (0-1)
     //float fade = sin(m / numPixels * PI);
+    // Splitting up RGB in bytes using bitshifting
     //int red = (c & 0xFF0000) >> 16;
     //int green = (c & 0xFF00) >> 8;
     //int blue = c & 0xFF;
@@ -89,6 +106,7 @@ void outerBlue(uint32_t c) {
     strip.setPixelColor(m, c);
     strip.show();
   }
+  // When DMX channel 1 is more than 100 then the blue noise appears
   if (bright > 100) {
     for (int k = 0; k < lowerBorder; k++) {
       strip.setPixelColor(k, random(c));
